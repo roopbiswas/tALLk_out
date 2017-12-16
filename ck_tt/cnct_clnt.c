b65752cfa86c67dc28fcc029b631eb378986bf44
Rituparno Biswas <rituparnobiswas@gmail.com>
2017-12-16 19:36:59 +0530

    Add files via upload

---
+++ ck_tt/cnct_clnt.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define request "request"
+#define logout "logout"
+#define conct "connect"
+#define wait "wait"
+#define kill "kill"
+#define die "die"
+
+/*
+	Structure to maintain the list of other users
+*/
+struct maintain
+{
+	int port;
+	char ip[100];
+}list [100];
+
+int ind = 0;	//index pointer for the list of other users
+int s, n;
+char rec_buffer[100],send_buffer[100];
+
+int *ptr[2];
+pthread_t tid[2];
+
+//Function to keep receiving the messages from server
+void * keep_receiving()
+{
+	while(1)
+	{
+		//Check if there is a message in the buffer
+		recv(s,&rec_buffer,sizeof(rec_buffer),MSG_PEEK);
+		if(strlen(rec_buffer) == 0)
+			continue;
+
+		//Get the message in the buffer
+		recv(s,&rec_buffer,sizeof(rec_buffer),0);
+
+		//If the message is "end"
+		if(strcmp(rec_buffer,"end")==0)
+		{
+			//Ask the server to close this chat session
+			strcpy(send_buffer,die);
+			send(s,&send_buffer,sizeof(send_buffer),0);
+
+			//Cancel the keep_sending thread
+			if(pthread_cancel(tid[1]) == 0)
+				printf("keep_receiving cancelled keep_sending");
+			else
+				printf("keep_receiving failed");
+			printf("\n");
+
+			//Exit this thread
+			int ret = 0;
+			pthread_exit(&ret);
+		}
+		printf("Reply > %s\n",rec_buffer);
+	}
+}
+
+//Function to keep sending message to server
+void * keep_sending()
+{
+	while(1)
+	{
+		//Wait for user input
+		printf("> : ");
+		scanf("%s", send_buffer);
+
+		//Ask the server to forward the message to other client
+		send(s,&send_buffer,sizeof(send_buffer),0);
+
+		//If the chat has to be ended
+		if(strcmp(send_buffer,"end")==0)
+		{
+			//Cancel the keep_receiving thread
+			if(pthread_cancel(tid[0]) == 0)
+				printf("keep_sending cancelled keep_receiving");
+			else
+				printf("keep_sending failed");
+			printf("\n");
+
+			//Exit this thread
+			memset(send_buffer,0,sizeof(send_buffer));
+			int ret = 0;
+			pthread_exit(&ret);
+		}
+	}
+}
+
+//Function to update the list of online users
+void update_list()
+{
+	send(s,&request,sizeof(request),0);
+	int num;
+	recv(s,&num,sizeof(num),0);
+	
+	ind = 0;
+	
+	//Updating the list of online users
+	printf("ID ---> ______IP______ : PORT\n");
+	while(num--)
+	{
+		char ip[100]; int port;
+		recv(s,&ip,sizeof(ip),0);
+		recv(s,&port,sizeof(port),0);
+		strcpy(list[ind].ip,ip);
+		list[ind].port = port;
+		printf("%d  ---> %-15s : %d\n", ind, ip, port);
+		ind++;
+	}
+}
+
+main()
+{
+	struct sockaddr_in client,server;
+
+	
+	//setting up the connection variables
+	s=socket(AF_INET,SOCK_STREAM,0);
+	server.sin_family=AF_INET;
+	server.sin_port=15518;
+	server.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+	printf("\nClient side has been setup successfully...\n");
+
+	n=sizeof(server);
+	
+	//connecting to the server
+	int status = connect(s,(struct sockaddr *)&server,n);
+
+	if(status == -1)
+	{
+		printf("Connection failure\n");
+		return 1;
+	}
+
+	while(1)
+	{
+		printf("\n\n1> Get the list of clients\n2> Connect to a client\n3> Wait for connection\n4> Logout\n\n");
+		
+		int dec;
+		scanf("%d", &dec);
+
+		if(dec == 1)	//requesting for a list of users
+		{
+			update_list();
+		}
+		else if(dec==2)		//connecting to an online user
+		{
+			update_list();
+			
+			send(s,&conct,sizeof(conct),0);
+			printf("\n\nEnter the id of client you want to connect to : ");
+
+			int conn;
+			scanf("%d", &conn);
+
+			if(conn >= ind)
+			{
+				printf("The client ID is wrong\n");
+				continue;
+			}
+
+			//sending server the info of the client we want to connect to
+			send(s,&list[conn].ip,sizeof(list[conn].ip),0);
+			send(s,&list[conn].port,sizeof(list[conn].port),0);
+
+			
+			pthread_create(&tid[0], NULL, keep_receiving, NULL);
+			pthread_create(&tid[1], NULL, keep_sending, NULL);
+			
+			if(pthread_join(tid[0],(void**)&(ptr[0])) != 0)
+				printf("pthread_join 1 error");
+			if(pthread_join(tid[1],(void**)&(ptr[0])) != 0)
+				printf("pthread_join 2 error");
+		}
+		else if(dec==3)		//go online
+		{
+			int conn;
+
+			//wait for a chat request
+			send(s,&wait,sizeof(wait),0);
+			recv(s,&conn,sizeof(conn),0);
+			printf("Got a chat request\n");
+			send(s,&conn,sizeof(conn),0);
+
+			pthread_create(&tid[0], NULL, keep_receiving, NULL);
+			pthread_create(&tid[1], NULL, keep_sending, NULL);
+			
+			if(pthread_join(tid[0],(void**)&(ptr[0])) != 0)
+				printf("pthread_join 1 error");
+			if(pthread_join(tid[1],(void**)&(ptr[0])) != 0)
+				printf("pthread_join 2 error");
+		}
+		else if(dec==4)	//logout from the server
+		{
+			send(s,&logout,sizeof(logout),0);
+			close(s);
+			break;
+		}
+		else
+		{
+			printf("Wrong choice\n");
+		}
+	}
+
+	return 0;
+}
---
+++ ck_tt/cnct_srvr.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <math.h>
+
+pthread_t tid[20];	//to create different threads
+int tind=0;			//to keep track of threads
+
+//structure to hold details of online clients
+struct user
+{
+	int port, socket;
+	char ip[100];
+}list [100];
+
+//index pointer for the list of clients
+int ind;
+
+void *foo(void * n_s)
+{
+	char rec_buffer[100], send_buffer[100];
+	struct user o = *(struct user *) n_s;
+	int new_socket = o.socket;
+	int i, j;
+
+	while(1)
+	{
+		printf("\nSERVER : Waiting for : %s %d\n", o.ip, o.port);
+		memset( rec_buffer, 0, sizeof(rec_buffer) );
+		recv( new_socket, &rec_buffer, sizeof(rec_buffer), 0 );
+
+		printf("\nSERVER : Proccessing request from : %s %d\n", o.ip, o.port);
+		printf("%s\n", rec_buffer);
+
+		//Client asked for a list of online users
+		if( strcmp( rec_buffer, "request" ) == 0 )
+		{
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+			printf("SERVER : Size of the list : %d\n", ind);
+
+			//Send the size of list
+			send( new_socket, &ind, sizeof(ind), 0 );
+
+			printf("The List : \n");
+
+			for(i=0; i<ind; i++)
+			{
+				printf("%s %d\n", list[i].ip, list[i].port);
+				if( strcmp( o.ip, list[i].ip) == 0 && o.port == list[i].port )
+					continue;
+
+				//Send the other client's ip and port number
+				send( new_socket, &list[i].ip, sizeof(list[i].ip), 0 );
+				send( new_socket, &list[i].port, sizeof(list[i].port), 0 );
+			}
+		}
+		//Client asked to connect to another client (target_client)
+		else if( strcmp(rec_buffer,"connect") == 0 )
+		{
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+			char rec_ip[100]; int rec_port;
+
+			//Get the ip and port of the target client
+			recv( new_socket, &rec_ip, sizeof(rec_ip), 0 );
+			recv( new_socket, &rec_port, sizeof(rec_port), 0 );
+
+			char this_ip[100]; 
+			int this_socket, this_port;
+
+			//Find the target_client in the list of online clients
+			//Remove the taget client from the list of online clients
+			for(i=0; i<ind;i++)
+			{
+				if(strcmp( rec_ip, list[i].ip) == 0 && rec_port == list[i].port)
+				{
+					this_socket = list[i].socket;
+					strcpy(this_ip,list[i].ip);
+					this_port = list[i].port;
+					for(j=i; j<ind; j++)
+					{
+						strcpy(list[j].ip,list[j+1].ip);
+						list[j].port = list[j+1].port;
+						list[j].socket = list[j+1].socket;
+					}
+					break;
+				}
+			}
+			ind--;
+
+			//Send target_client details of this client
+			send( this_socket, &new_socket, sizeof(new_socket), 0 );
+
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+			printf("SERVER : Starting chat at the connect side\n");
+
+			//Chatting
+			while(1)
+			{
+				recv( new_socket, &rec_buffer, sizeof(rec_buffer), 0 );
+
+				if(strcmp(rec_buffer,"die")==0)
+					break;
+
+				send( this_socket, &rec_buffer, sizeof(rec_buffer), 0 );
+				if(strcmp(rec_buffer,"end")==0)
+					break;
+			}
+
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+
+			printf("SERVER : chat was closed at the connect side\n");
+
+		}
+		//Client wants to wait for an incoming connection
+		//This Client goes online
+		else if( strcmp(rec_buffer,"wait") == 0 )
+		{
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+
+			//Add this client to online list
+			strcpy(list[ind].ip, o.ip);
+			list[ind].port = o.port;
+			list[ind].socket = o.socket;
+			ind++;
+
+			char this_ip;
+			int this_port, this_socket;
+			printf("SERVER : Waiting for a chat request\n");
+			
+			//Client informed it's server instance about the client who is
+			//trying to chat
+			recv( new_socket, &this_socket, sizeof(this_socket), 0 );
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+			printf("SERVER : Got a chat request\n");
+
+			//Chatting
+			while(1)
+			{
+				recv( new_socket, &rec_buffer, sizeof(rec_buffer), 0 );
+
+				if(strcmp(rec_buffer,"die")==0)
+					break;
+
+				send( this_socket, &rec_buffer, sizeof(rec_buffer), 0 );
+				if(strcmp(rec_buffer,"end")==0)
+					break;
+			}
+
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+
+			printf("SERVER : Chat was closed at the wait side\n");
+
+		}
+		//Client wants to logout from the server
+		else if(strcmp(rec_buffer,"logout") == 0)
+		{
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+			printf("SERVER : Closing connection and removing from the list\n");
+			close(new_socket);
+			return 0;
+		}
+		//In case of some garbage message
+		else
+		{
+			printf("SERVER : Something Wrong here\n");
+			printf("\nSERVER : AT : %s %d\n", o.ip, o.port);
+			printf("SERVER : Closing connection and removing from the list\n");
+			close(new_socket);
+			return 0;
+		}
+	}
+}
+
+main()
+{
+	struct sockaddr_in client,server;
+
+	int n, s;
+
+	char rec_buffer[100], send_buffer[100];
+
+	s = socket( AF_INET, SOCK_STREAM, 0 );
+	server.sin_family = AF_INET;
+	server.sin_port = 15518;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	if ( bind( s, (struct sockaddr *)&server, sizeof(server) ) == -1 )
+	{
+		printf("Binding failed..\n");
+		return 1;
+	}
+
+	printf("\nServer was started successfully.. waiting for response from client...\n");
+
+	n = sizeof(client);
+
+	int status = listen( s, 2 );
+
+	struct user obj;
+
+	while(1)
+	{
+		
+		int new_socket = accept(s,(struct sockaddr *)&client,&n);
+		printf("SERVER : Got a new connection from -- %s %d\n\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+		
+		
+		obj.socket = new_socket;
+		obj.port = ntohs(client.sin_port);
+		strcpy(obj.ip,inet_ntoa(client.sin_addr));
+
+		pthread_create( &(tid[tind]), NULL, foo, (void *)&obj );
+		tind++;
+	}
+
+	close(s);
+
+	return 0;
+}
